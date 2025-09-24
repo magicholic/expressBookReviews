29d3b99a10b4a5b9bf0aba62fb1a0a26dae70803
Fan Gao <fancrazy88@gmail.com>
2025-09-24 15:04:18 -0600

    task 3 is done

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -23,8 +23,17 @@ public_users.get('/isbn/:isbn',function (req, res) {
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let author = req.params.author;
+  let results = [];
+  for (let isbn in books) {
+    if (books[isbn].author === author) {
+      results.push(books[isbn]);
+    }
+  }
+  if(results.length === 0) {
+    return res.status(404).json({message: "Author not found"});
+  }
+  res.send(results);
 });
 
 // Get all books based on title
