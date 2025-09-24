e8bbc9fafbcde0a3217a7b91fa021213fa0dbe39
Fan Gao <fancrazy88@gmail.com>
2025-09-24 15:12:41 -0600

    task 4 is done

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -38,8 +38,17 @@ public_users.get('/author/:author',function (req, res) {
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let title = req.params.title;
+  let results = [];
+  for (let isbn in books) {
+    if (books[isbn].title === title) {
+      results.push(books[isbn]);
+    }
+  }
+  if(results.length === 0) {
+    return res.status(404).json({message: "Title not found"});
+  }
+  res.send(results);
 });
 
 //  Get book review
