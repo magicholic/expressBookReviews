a828c0c6d9f7ee3bb659b7a327674b4cc0fe9b42
Fan Gao <fancrazy88@gmail.com>
2025-09-24 18:59:18 -0600

    task 7 is done

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -22,8 +22,20 @@ const authenticatedUser = (username,password)=>{ //returns boolean
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let username = req.body.username;
+  let password = req.body.password;
+
+  if (isValid(username) && authenticatedUser(username,password)) {
+    let accessToken = jwt.sign({
+      data: password
+    }, 'access', { expiresIn: 60 * 60 });
+    req.session.authorization = {
+      accessToken,username
+    }
+    return res.status(200).json({message: "User successfully logged in"});
+  } else {
+    return res.status(208).json({message: "Invalid Login. Check username and password"});
+  }
 });
 
 // Add a book review
