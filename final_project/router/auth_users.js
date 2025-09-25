8bbcce0d5aa3992dd0e8bc923cb714b31d4518f3
Fan Gao <fancrazy88@gmail.com>
2025-09-25 11:46:46 -0600

    task 8 is done

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -29,9 +29,11 @@ regd_users.post("/login", (req,res) => {
     let accessToken = jwt.sign({
       data: password
     }, 'access', { expiresIn: 60 * 60 });
+
     req.session.authorization = {
       accessToken,username
     }
+
     return res.status(200).json({message: "User successfully logged in"});
   } else {
     return res.status(208).json({message: "Invalid Login. Check username and password"});
@@ -40,8 +42,16 @@ regd_users.post("/login", (req,res) => {
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let isbn = req.params.isbn;
+  let review = req.body.review;
+  let username = req.session.authorization["username"];
+
+  if (books[isbn]) {
+      books[isbn]["reviews"][username] = review;
+      return res.status(200).json({message: "Book review added/updated successfully"});
+  } else {
+    return res.status(404).json({message: "Book not found"});
+  }
 });
 
 module.exports.authenticated = regd_users;
